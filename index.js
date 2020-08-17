3287e0c2314f95ed970f26acc0292b0dedc2b9ac
VitalikVoicix <voicix1502@gmail.com>
2020-08-17 23:34:05 +0300

    fixed problem

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ let firstLetter=document.querySelector(".btn1")
 firstLetter.addEventListener('click',function(){
    firstLetter.innerHTML="<p>'S'</p>";
    firstLetter.classList.add("btn");
-   playSound("music/file1.mp3");
+   playSound("music/file9.mp3");
 })
 let secondLetter=document.querySelector(".btn2")
 secondLetter.addEventListener('click',function(){
@@ -47,7 +47,7 @@ let sevenLetter=document.querySelector(".btn7")
 sevenLetter.addEventListener('click',function(){
    sevenLetter.innerHTML="<p>'O'</p>";
    sevenLetter.classList.add("btn");
-   playSound("music/file7.mp3");
+   playSound("music/file3.mp3");
 })
 let eightLetter=document.querySelector(".btn8")
 eightLetter.addEventListener('click',function(){
@@ -61,3 +61,22 @@ nineLetter.addEventListener('click',function(){
    nineLetter.classList.add("btn");
    playSound("music/file9.mp3");
 })
+
+function playMusic(e) {
+   if(e.keyCode === 83) {
+      playSound("music/file1.mp3")
+   } else if(e.keyCode === 67) {
+      playSound("music/file2.mp3")
+   } else if(e.keyCode === 79) {
+      playSound("music/file3.mp3")
+   } else if(e.keyCode === 82) {
+      playSound ("music/file4.mp3")
+   } else if(e.keyCode === 80) {
+      playSound("music/file5.mp3")
+   } else if(e.keyCode === 73) {
+      playSound("music/file6.mp3")
+   }else if(e.keyCode === 78){
+      playSound("music/file9.mp3")
+   }
+}
+addEventListener("keydown", playMusic);
